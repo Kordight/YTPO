cdbb0abb8c119646d23394ed6de4b1c7003d2162
Kordight <164764949+Kordight@users.noreply.github.com>
2024-08-18 13:16:50 +0200

    Changed data format in html report

--- html_manager.py
+++ html_manager.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-today = datetime.today().strftime("%H-%M_%Y-%m-%d")
+today = datetime.today().strftime("%Y-%m-%d_%H:%M")
 
 # Function to generate HTML code for the list of similar songs
 def generate_html_duplicate_list(songs, playlist_name, playlist_url):
